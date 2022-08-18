1a6336a7f687e53e0d5cdf7321c5bdcd3d8176ff
co.abu <anizomxoja@gmail.com>
2022-08-18 08:59:10 +0500

    Initial Commit

---
+++ ContactMVVM/ContactMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactMVVMApp.swift
+//  ContactMVVM
+//
+//  Created by Avaz Mukhitdinov on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
